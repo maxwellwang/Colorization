23fe20a4c3a2af3b45580175728cc25d8e0e2633
maxwellwang <maxwellwang01@gmail.com>
2021-04-22 17:50:46 -0400

    Added show image functionality, but recolored train image still looks bad

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ from util import *
 
 if __name__ == '__main__':
     # convert image into 2d array of pixels
-    data = get_data('pool.jpeg')
+    data = get_data('cherry.jpg')
     # split left side into train and right side into test
     train_data, test_data = split(data)
     # get the 5 central colors on train side
     five_colors = cluster(train_data)
     # recolor the train side using the 5 central colors
-    recolored_train_data = round_colors(train_data, five_colors)
+    recolored_train_data = recolor(train_data, five_colors)
     # get gray version of data
-    gray_data = get_gray_data(data)
+    # gray_data = get_gray_data(data)
     # split left side into train and right side into test
-    train_gray_data, test_gray_data = split(gray_data)
-    print(train_gray_data)
-    print(test_gray_data)
+    # train_gray_data, test_gray_data = split(gray_data)
+    show_image(train_data)
+    show_image(recolored_train_data)
--- util.py
+++ util.py
@@ -1,27 +1,21 @@
 from PIL import Image
+import matplotlib.pyplot as plt
 from numpy import asarray
 from sklearn.cluster import KMeans
 
 
 def get_data(path):
     img = Image.open(path)
-    return asarray(img)
+    return list(asarray(img))
 
 
 def split(data):
     train_data = []
     test_data = []
-    for i in range(len(data)):
-        train_row = []
-        test_row = []
-        for j in range(len(data[0])):
-            if j <= len(data) / 2:
-                train_row.append(data[i][j])
-            else:
-                test_row.append(data[i][j])
-        train_data.append(train_row)
-        test_data.append(test_row)
-    return asarray(train_data), asarray(test_data)
+    for row in data:
+        train_data.append(row[:round(len(row) / 2)])
+        test_data.append(row[round(len(row) / 2):])
+    return train_data, test_data
 
 
 def cluster(train_data):
@@ -33,29 +27,25 @@ def cluster(train_data):
     centers = []
     for center in kmeans.cluster_centers_:
         centers.append([round(center[0]), round(center[1]), round(center[2])])
-    return asarray(centers)
+    return centers
 
 
 def closest_color(pixel, five_colors):
     smallest_diff = 255 * 3
-    index = 0
-    for i in range(len(five_colors)):
-        color = five_colors[i]
+    closest = five_colors[0]
+    for color in five_colors:
         diff = abs(pixel[0] - color[0]) + abs(pixel[1] - color[1]) + abs(pixel[2] - color[2])
         if diff < smallest_diff:
-            index = i
+            closest = color
             smallest_diff = diff
-    return five_colors[index]
+    return closest
 
 
-def round_colors(train_data, five_colors):
+def recolor(train_data, five_colors):
     recolored = []
     for row in train_data:
-        recolored_row = []
-        for pixel in row:
-            recolored_row.append(closest_color(pixel, five_colors))
-        recolored.append(recolored_row)
-    return asarray(recolored)
+        recolored.append([closest_color(pixel, five_colors) for pixel in row])
+    return recolored
 
 
 def get_gray_data(data):
@@ -65,4 +55,15 @@ def get_gray_data(data):
         for pixel in row:
             gray_data_row.append(round(0.21 * pixel[0] + 0.72 * pixel[1] + 0.07 * pixel[2]))
         gray_data.append(gray_data_row)
-    return asarray(gray_data)
+    return gray_data
+
+
+def show_image(data):
+    img = Image.fromarray(asarray(data), 'RGB')
+    img.show()
+
+
+def show_colors(colors):
+    for i, color in enumerate(colors):
+        plt.scatter(i + 1, 1, color=tuple([x / 255 for x in color]))
+    plt.show()
